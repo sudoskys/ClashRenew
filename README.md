f9460298df4730fcdc9f36f588cf2d1ac7f50bff
sudoskys <coldlando@hotmail.com>
2022-07-13 22:13:45 +0800

    new

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # ClashRenew
-Linux clash或clash-meta 配置-config.yaml 自动更新小脚本，使用前请审计代码，否则后果自负
+
+Linux clash或clash-meta 配置-config.yaml 自动更新小脚本，使用前请审计代码。
+
+
+**使用本项目造成的后果需使用者负责。**
+
+
+### 依赖
+
+```pip install pyyaml==5.4.1```
+
+>因为 yaml 库的兼容问题，需要使用 5.4.1，如果不是可以使用 ```pip unstall```
+
+### 方式一: 配置开机自动
+
+使用```.zshrc```或者```.bashrc```即可
+
+
+### 方式二: 配置定时运行
+
+```
+EDITOR=vim crontab -e
+```
+
+**输入定时任务**
+每五小时执行一次（因为你不太可能开着机）
+ ```0 */5 * * *  /bin/sh ～/cron.sh```
+
+**查看定时任务**
+```
+crontab -l
+```
+
+
+
+### 程序配置文件示例
+
+注意空格
+```
+RenewConfig: True
+RenewTargetKey: cloud22
+content:
+   cloud1:
+       - https://
+   cloud22:
+       - https://
+
+```
+
+```RenewConfig``` 指是否替换运行依赖的config,```RenewTargetKey```指替换的目标（必须在列表里）
+
+
+
+
+编辑 python 脚本，按照注释配置 配置文件名称 和 数据目录。
---
+++ clash.py
@@ -0,0 +1,89 @@
+# encoding: utf-8
+# auto clash config file renew prog
+
+# config?
+Name="randomname.yaml"
+
+#data
+Folder="Clash"
+
+
+import os
+import requests
+import yaml
+import time
+
+
+targetPath = os.getcwd()+ "/" + Name
+print("使用配置文件: "+targetPath)
+
+
+class App(object):
+   def __init__(self, C) -> None:
+       if not C == "":
+         self.count=[]
+         self.c = yaml.load(C)
+         self.aR=self.c.get('RenewConfig')
+         self.target=self.c.get('RenewTargetKey')
+         self.content=self.c.get("content")
+       else:
+         self.c = False
+       self.Cpath=os.getcwd()+ "/" +Folder
+       folder = os.path.exists(self.Cpath)
+       if not folder:
+          os.makedirs(self.Cpath)
+          print("Create New Folder:" +self.Cpath)
+
+   def run(self):
+       if all([self.c,self.aR,self.target,self.content]):
+          for k,v in self.content.items():
+              self.Worker(k,v)
+          self.Finally()
+       else:
+          print("Error: Argument missing or invalid")
+   def Worker(self, A, B):
+       try:
+          r=requests.get(B[0])
+          r.raise_for_status()  #如果不是200，产生异常requests.HTTPError
+          r.encoding=r.apparent_encoding
+          rpath=self.Cpath+'/'+str(A)
+          if not os.path.exists(rpath):
+            os.makedirs(rpath)
+          if len(r.text)>500:
+            with open(rpath +"/"+time.strftime("%Y%m%d%H%M%S")+'.yaml', 'w+') as f:
+                f.write(r.text)
+                f.flush()
+                os.fsync(f.fileno())
+          else:
+            raise ValueError("Content less than 500 words, skip for safety")
+       except Exception as e:
+          print("Error Request..."+str(e))
+       else:
+          self.count.append(A)
+          if A == self.target:
+             try:
+                with open(os.getcwd() +"/.config/clash/config.yaml", 'w+') as f:
+                   f.write(r.text)
+                   f.flush()
+                   os.fsync(f.fileno())
+             except Exception as e:
+                print("Error updating run file "+str(e))
+
+   def Finally(self):
+       print("Update completed-"+time.strftime("%Y-%m-%d %H:%M:%S")+" Total:"+str(len(self.content)) + " ,Succesful Renew:"+str(len(self.count)))
+       if self.aR:
+          print("The running configuration file has been replaced ,using"+self.target)
+
+
+# main
+try:
+    f = open(targetPath, 'r')
+except:
+    print("IO Error： Pleas Make sure that file really exist!")
+else:
+    content = f.read()
+    f.close()
+    App(content).run()
+
+
+
